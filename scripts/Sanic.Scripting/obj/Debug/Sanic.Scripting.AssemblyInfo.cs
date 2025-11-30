326bbb5be279094b69bbbc82fecd274e21097ee0
Deb Kumar <59546212+MegaPanchamZ@users.noreply.github.com>
2025-11-30 11:41:07 +1100

    fix: Update AssemblyInformationalVersion and cache input hash for Sanic.Scripting

--- scripts/Sanic.Scripting/obj/Debug/Sanic.Scripting.AssemblyInfo.cs
+++ scripts/Sanic.Scripting/obj/Debug/Sanic.Scripting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sanic.Scripting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d97fc8c05375593df7cf9f9ef68e6f6c71ccc58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc893dc8b75d1dfc5885c937024765d309968117")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sanic.Scripting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sanic.Scripting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
